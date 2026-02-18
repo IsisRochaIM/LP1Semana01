ae043520d82ed3bb1e45900d7e36b55aa375e601
Isis <mejaikatwt@gmail.com>
2026-02-18 16:08:45 +0000

    Add integers

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+            int vallue1 = 6;
+            short vallue2 = 8;
+            long vallue3 = 10;
+            Console.WriteLine(vallue1);
+            Console.WriteLine(vallue2);
+            Console.WriteLine(vallue3);
+            
+        }
+    }
+}
