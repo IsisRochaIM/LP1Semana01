d36108cac2382fd51a1cb450062d40ebca6048f5
Isis <mejaikatwt@gmail.com>
2026-02-18 15:51:09 +0000

    Add documentation

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// documentação 1
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// documentação 2
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
