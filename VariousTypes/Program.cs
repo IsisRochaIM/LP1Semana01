60fa2fee03fec20a39aaa15dea8d02f95d687331
Isis <mejaikatwt@gmail.com>
2026-02-18 16:31:10 +0000

    Add chars

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -13,6 +13,14 @@ namespace VariousTypes
             Console.WriteLine(vallue1);
             Console.WriteLine(vallue2);
             Console.WriteLine(vallue3);
+
+            char char1n = '\uF31E';
+            char char2 = '\uF438';
+            char char3 = '\uF36E';
+            Console.WriteLine(char1n);
+            Console.WriteLine(char2);
+            Console.WriteLine(char3);
+            
             
         }
     }
