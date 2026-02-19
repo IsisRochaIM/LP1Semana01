4d70b7d8fe7e86031b498417bd241c2b1b17a040
Isis <mejaikatwt@gmail.com>
2026-02-19 15:17:40 +0000

    add strigns with unicode and scape

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+            
+            string basicString = "Pão de Laranja";
+            string specialString = "banana\\\nmaçã\\\n\"abacate\"";
+            string unicodeString = "\u03A9";
+
+            Console.WriteLine(basicString);
+            Console.WriteLine(specialString);
+            Console.WriteLine(unicodeString);
+
+            
+        }
+    }
+}
