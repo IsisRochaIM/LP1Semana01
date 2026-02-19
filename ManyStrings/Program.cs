a7fced85e70a95150eb389d740f42e08694b05e6
Isis <mejaikatwt@gmail.com>
2026-02-19 15:35:54 +0000

    add strigns with interpolation and [200~

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace ManyStrings
 {
@@ -16,7 +17,20 @@ namespace ManyStrings
             Console.WriteLine(specialString);
             Console.WriteLine(unicodeString);
 
-            
+            string s1 = "palavra " + 4;
+            string s2 = "Eu gosto de " + basicString;
+            int vallue4 = 5 + 8;
+            int vallue5 = 8 - 3; 
+            string s3 = $"A soma de x e y é {vallue4 + vallue5}";
+            string s4 = $"O valor de x é {vallue4} que é a soma de 5 e 8";
+            string s5 = $"O valor de y é {vallue5} que é a subtração de 8 por 3";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+                        
         }
     }
 }
