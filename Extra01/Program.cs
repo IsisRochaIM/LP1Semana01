d5dd478579fedab1993c31a4565fa7843487cdb2
Isis <mejaikatwt@gmail.com>
2026-02-19 16:43:05 +0000

    rename project

---
+++ Extra01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace E01
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+            string registredChar = "\\u00AE";
+            Console.WriteLine(registredChar);
+        }
+    }
+}
